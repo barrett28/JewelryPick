3b3d003e06fd21e2c05fc8286e6ec43018e95a26
Bharat <bharat2808singh@gmail.com>
2025-07-08 17:58:40 +0530

    home and about

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Home from "./components/Home";
+import Update from "./components/Update";
+
+const App = () => {
+  return (
+    <div>
+      <Home />
+      <Update />
+    </div>
+  );
+};
+
+export default App;
